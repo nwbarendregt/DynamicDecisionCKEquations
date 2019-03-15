4c25722bb919fd8343ac96cc8d025880d99109a9
nwbarendregt <48600084+nwbarendregt@users.noreply.github.com>
2019-03-15 11:57:17 -0600

    Update Clicks_Linear_Accuracy_Statistics_Gamma.m

--- Figure_8/Clicks_Linear_Accuracy_Statistics_Gamma.m
+++ Figure_8/Clicks_Linear_Accuracy_Statistics_Gamma.m
@@ -1,19 +1,19 @@
-% Used to generate Fig. 7B from the manuscript.
+% Used to generate Fig. 7A from the manuscript.
 clear
 
 t_0 = 0; dt = 0.01; t_f = 1; t = t_0:dt:t_f;
 b = 50; dy = 0.01; y = -b:dy:b;
-kappa = [0.24 0.46 0.66 0.85]; lambda_m = 30; D = 0.05; gamma = linspace(0.1,8);
+kappa = [0.24 0.46 0.66 0.85]; r_m = 30; D = 0.05; lambda = linspace(0.1,8);
 h_o = 1; h_t = 1;
 
-acc = NaN(length(kappa),length(gamma));
+acc = NaN(length(kappa),length(lambda));
 
 for i = 1:length(kappa)
-    for j = 1:length(gamma)
-        [acc(i,j),~] = Clicks_Linear_Accuracy_Interrogation(kappa(i),lambda_m,gamma(j),D,h_o,h_t,t_0,dt,t_f,b,dy);
+    for j = 1:length(lambda)
+        [acc(i,j),~] = Clicks_Linear_Accuracy_Interrogation(kappa(i),r_m,lambda(j),D,h_o,h_t,t_0,dt,t_f,b,dy);
     end
     disp(i)
 end
-save('Clicks_Linear_Accuracy_Statistics_Gamma_Data');
+save('Clicks_Linear_Accuracy_Statistics_Lambda_Data');
 
-plot(gamma,acc,'linewidth',5)
+plot(lambda,acc,'linewidth',5)
